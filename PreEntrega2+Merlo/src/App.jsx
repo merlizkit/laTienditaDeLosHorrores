93f699dc683492e4960485c04b7512d207d8809b
merlizkit <merlizkit@gmail.com>
2023-09-11 03:13:45 -0300

    Tab Icon

--- PreEntrega2+Merlo/src/App.jsx
+++ PreEntrega2+Merlo/src/App.jsx
@@ -1,6 +1,5 @@
 //import './App.css'
 import './style.css'
-import { useEffect, useState } from "react";
 import NavBar from './components/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
