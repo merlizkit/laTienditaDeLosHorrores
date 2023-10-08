e44d7dc00066ad5aacb56a855818c6d5a6876a7b
merlizkit <merlizkit@gmail.com>
2023-10-07 21:33:34 -0300

    Sacar ternario

--- EntregaFinal+Merlo/src/components/Item.jsx
+++ EntregaFinal+Merlo/src/components/Item.jsx
@@ -6,17 +6,13 @@ export const Item = ({data}) => {
 
     return (
         <>
-            {
-                data ? (
-                    <div className="item">
-                        <NavLink className="item-card" key={data.id} to={`/item/${data.id}`} >
-                            <img className="item-card-img" src={data.image} alt="product-image" />
-                            <h5 className="item-card-title">{data.title}</h5>
-                            <p className="item-card-price">USD {data.price}</p>
-                        </NavLink>
-                    </div>
-                ) : null
-            }
+            <div className="item">
+                <NavLink className="item-card" key={data.id} to={`/item/${data.id}`} >
+                    <img className="item-card-img" src={data.image} alt="product-image" />
+                    <h5 className="item-card-title">{data.title}</h5>
+                    <p className="item-card-price">USD {data.price}</p>
+                </NavLink>
+            </div>
         </>
     )
 }
